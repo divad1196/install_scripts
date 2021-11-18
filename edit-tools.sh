602599a89d17234ff8cfe2005ec18c23f58ad22c
David Gallay <davidtennis96@hotmail.com>
2021-11-18 09:11:24 +0100

    first commit

---
+++ edit-tools.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt install -y ardour                              # Adobe Audition
+sudo apt install -y kdenlive openshot                   # Adobe Premiere
+sudo apt install -y darktable                           # Adobe Lightroom
+sudo apt install -y synfig                              # Adobe Animate
+sudo apt install -y inkscape            # scribus       # Adobe Indesign
+snap install natron                     # buttleofx ?   # Adobe Aftereffects
+
+sudo apt install -y krita blender  # Rendering
+
+# wget https://github.com/NatronGitHub/Natron/releases/download/v2.4.1/Natron-2.4.1-Linux-x86_64.tgz
+# tar -xvzf Natron-2.4.1-Linux-x86_64.tgz
+# chmod +x Natron-2.4.1-Linux-x86_64
+# ./Natron-2.4.1-Linux-x86_64
